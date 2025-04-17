aae541c1ca12a1fe47e2a655e1b25b339ed9c005
Sharan Ganti <gantisharan6639@gmail.com>
2025-04-17 22:36:17 +0530

    model trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig():
@@ -59,4 +61,10 @@ if __name__ == "__main__":
     train_data_path, test_data_path = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+    
+    model_trainer = ModelTrainer()
+    
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+    
+    
--- src/components/test.py
+++ src/components/test.py
@@ -1,6 +1,13 @@
-import pandas as pd
 
-raw_data = pd.read_csv('/Users/sharan/Documents/ML Study/MLProject/artifacts/raw.csv')
-numerical_columns = raw_data.select_dtypes(exclude="object").columns.tolist()
-cat_columns = raw_data.select_dtypes(include="object").columns.tolist()
 
+x = {'a' : 1,
+     'b' : -2,
+     'c' : 4}
+
+z = max(x.values())
+
+y = list(x.keys())[
+    list(x.values()).index(z)
+]
+
+print(y)
